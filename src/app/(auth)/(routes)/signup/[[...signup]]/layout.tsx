ca4a075514276abe203a3c3d69874dfb75cd84cf
Honey -- <hoorulain708@gmail.com>
2024-05-21 00:04:21 +0500

    initial commit

---
+++ src/app/(auth)/(routes)/signup/[[...signup]]/layout.tsx
@@ -0,0 +1,21 @@
+import type {  Metadata } from "next";
+import NextTopLoader from "nextjs-toploader";
+
+export const metadata: Metadata = {
+    title:"Sign Up | short URL",
+    description:"Honey's web",
+
+}
+const SignUpLayout = ({
+    children,
+}:{
+    children: React.ReactNode
+})=>{
+    return(
+        <>
+        <NextTopLoader color="#000" showSpinner = {false}/>
+        {children}
+        </>
+    )
+}
+export default SignUpLayout;
